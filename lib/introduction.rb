35a66099c8d1ec2af4c30350dc7fa8a3c874f415
arnold bwaila <arnold.bwaila@gmail.com>
2019-07-29 18:21:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,11 @@
 def introduction(name)
-  return "Hi my name is #{name}."
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  return "Hi my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional(name, language = "Ruby")
-  return "Hi my name is #{name} and I am learning to program in #{language}."
+  puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
